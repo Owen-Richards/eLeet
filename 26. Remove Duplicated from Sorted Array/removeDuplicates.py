9a6d985001135dab5c34f2f02eba10ed64031c38
Owen R <owenlrichards2000@gmail.com>
2024-01-06 20:35:42 -0500

    daily leet

---
+++ 26. Remove Duplicated from Sorted Array/removeDuplicates.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums[:] = sorted(set(nums)) # The reason for [:] is because it replaces the elements form the original list per requirements
+        return len(nums)
+
+
+class Solution:
+    def removeDuplicates(self, nums) -> int:
+        j = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[j] = nums[i]
+                j += 1
+        return j
